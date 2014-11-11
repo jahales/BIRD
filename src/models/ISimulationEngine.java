7a91fbc849797a9b299af921435d77ebc992f8a8
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-11 00:38:13 -0700

    Simulators stubbed

---
+++ src/models/ISimulationEngine.java
@@ -0,0 +1,18 @@
+package models;
+
+/**
+ * 
+ * @author Brian Woodruff
+ *
+ */
+public class ISimulationEngine {
+    /**
+     * Run the simulation
+     * 
+     * @param simulation
+     * @return
+     */
+    public String run(Simulation simulation) {
+	return null;
+    }
+}
---
+++ src/models/Simulation.java
@@ -0,0 +1,70 @@
+package models;
+
+/**
+ * Simulation class
+ * 
+ * @author Brian Woodruff
+ *
+ */
+public class Simulation {
+    private String rocketFile;
+    private String engineFile;
+    private String atmosphereFile;
+    private String launchRailFile;
+    
+    /**
+     * @return rocketFile
+     */
+    public String getRocketFile() {
+        return rocketFile;
+    }
+    
+    /**
+     * @param rocketFile
+     */
+    public void setRocketFile(String rocketFile) {
+        this.rocketFile = rocketFile;
+    }
+    
+    /**
+     * @return engineFile
+     */
+    public String getEngineFile() {
+        return engineFile;
+    }
+    
+    /**
+     * @param engineFile
+     */
+    public void setEngineFile(String engineFile) {
+        this.engineFile = engineFile;
+    }
+    
+    /**
+     * @return atmosphereFile
+     */
+    public String getAtmosphereFile() {
+        return atmosphereFile;
+    }
+    
+    /**
+     * @param atmosphereFile
+     */
+    public void setAtmosphereFile(String atmosphereFile) {
+        this.atmosphereFile = atmosphereFile;
+    }
+    
+    /**
+     * @return launchRailFile
+     */
+    public String getLaunchRailFile() {
+        return launchRailFile;
+    }
+    
+    /**
+     * @param launchRailFile
+     */
+    public void setLaunchRailFile(String launchRailFile) {
+        this.launchRailFile = launchRailFile;
+    }
+}
