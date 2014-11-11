cd11e930f483885a1d62744194ac8f6fcae3dd85
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-10 22:36:55 -0700

    An idea to define a view for each type of part?

---
+++ src/controllers/FinController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * A controller for a fin part.
+ * 
+ * @author Brian Woodruff
+ *
+ */
+public class FinController {
+
+    @FXML
+    private TextField finCountValue;
+
+    @FXML
+    private TextField spanLengthValue;
+
+    @FXML
+    private TextField spanLengthError;
+
+    @FXML
+    private TextField sweepLengthValue;
+
+    @FXML
+    private TextField sweepLengthError;
+
+    @FXML
+    private TextField rootChordError;
+
+    @FXML
+    private TextField rootChordValue;
+
+}
