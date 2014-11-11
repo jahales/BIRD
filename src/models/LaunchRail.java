6ea3bc4ce14d7a97d5bb8ad0186be50a05baa121
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-11 00:39:26 -0700

    Added launch rail and measurement classes.

---
+++ src/models/LaunchRail.java
@@ -0,0 +1,55 @@
+package models;
+
+/**
+ * A laumch rail
+ * 
+ * @author Brian Woodruff
+ *
+ */
+public class LaunchRail {
+    private Measurement length;
+    private Measurement polarAngle;
+    private Measurement azimuthAngle;
+    
+    /**
+     * @return length
+     */
+    public Measurement getLength() {
+        return length;
+    }
+    
+    /**
+     * @param length
+     */
+    public void setLength(Measurement length) {
+        this.length = length;
+    }
+    
+    /**
+     * @return polarAngle
+     */
+    public Measurement getPolarAngle() {
+        return polarAngle;
+    }
+    
+    /**
+     * @param polarAngle
+     */
+    public void setPolarAngle(Measurement polarAngle) {
+        this.polarAngle = polarAngle;
+    }
+    
+    /**
+     * @return azimuthAngle
+     */
+    public Measurement getAzimuthAngle() {
+        return azimuthAngle;
+    }
+    
+    /**
+     * @param azimuthAngle
+     */
+    public void setAzimuthAngle(Measurement azimuthAngle) {
+        this.azimuthAngle = azimuthAngle;
+    }
+}
---
+++ src/models/Measurement.java
@@ -0,0 +1,55 @@
+package models;
+
+/**
+ * A Measurement class container.
+ * 
+ * @author Brian Woodruff
+ *
+ */
+public class Measurement {
+    private double value;
+    private double error;
+    private double Enum;
+    
+    /**
+     * @return value
+     */
+    public double getValue() {
+        return value;
+    }
+    
+    /**
+     * @param value
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+    
+    /**
+     * @return error
+     */
+    public double getError() {
+        return error;
+    }
+    
+    /**
+     * @param error
+     */
+    public void setError(double error) {
+        this.error = error;
+    }
+    
+    /**
+     * @return Enum
+     */
+    public double getEnum() {
+        return Enum;
+    }
+    
+    /**
+     * @param enum1
+     */
+    public void setEnum(double Enum) {
+        this.Enum = Enum;
+    }
+}
