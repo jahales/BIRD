d398944e3374715e260ef230e2fcea9259e1c0ce
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-12 15:55:20 -0700

    Changed tab and whitespace mostly.

--- src/models/report/DataTable.java
+++ src/models/report/DataTable.java
@@ -10,50 +10,51 @@ import java.util.Map;
  *
  */
 public class DataTable {
-    private List<String> columnNames;
-    private Map<String, List<Double>> columnMap;
-    private List<List<Number>> data; // List of rows
-    
-    /**
-     * @return columnNames
-     */
-    public List<String> getColumnNames() {
-        return columnNames;
+  private List<String> columnNames;
+  private Map<String, List<Double>> columnMap;
+  private List<List<Number>> data; // List of rows
+
+  /**
+   * @return columnNames
+   */
+  public List<String> getColumnNames() {
+    return columnNames;
+  }
+
+  /**
+   * @param columnNames
+   */
+  public void setColumnNames(List<String> columnNames) {
+    this.columnNames = columnNames;
+  }
+
+  /**
+   * @return columnMap
+   */
+  public Map<String, List<Double>> getColumnMap() {
+    return columnMap;
+  }
+
+  /**
+   * @return columns
+   */
+  public List<List<Number>> getData() {
+    return data;
+  }
+
+  public void addRow(List<Number> row) throws RowFormatError {
+    if (row.size() == columnNames.size()) {
+      data.add(row);
+    } else {
+      throw new RowFormatError("Size of row does match number of columns defined.");
     }
-    
-    /**
-     * @param columnNames
-     */
-    public void setColumnNames(List<String> columnNames) {
-        this.columnNames = columnNames;
+  }
+
+  public class RowFormatError extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public RowFormatError(String error) {
+      super(error);
     }
-    
-    /**
-     * @return columnMap
-     */
-    public Map<String, List<Double>> getColumnMap() {
-        return columnMap;
-    }
-    
-    /**
-     * @return columns
-     */
-    public List<List<Number>> getData() {
-        return data;
-    }
-    
-    public void addRow(List<Number> row) throws RowFormatError  {
-	if (row.size() == columnNames.size()) {
-	    data.add(row);
-	} else {
-	    throw new RowFormatError("Size of row does match number of columns defined.");
-	}
-    }
-    
-    public class RowFormatError extends Exception {
-	private static final long serialVersionUID = 1L;
-	public RowFormatError(String error) {
-	    super(error);
-	}
-    }
+  }
 }
