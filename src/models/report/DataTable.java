140e1bb33674519030651de868ac9806c28044d7
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-12 16:17:23 -0700

    Add row added. Add rows removed. CSVReader updated.

--- src/models/report/DataTable.java
+++ src/models/report/DataTable.java
@@ -10,9 +10,62 @@ import java.util.Map;
  *
  */
 public class DataTable {
+  /**
+   * Exception class of my own. Don't know if I really want it though.
+   * 
+   * @author Brian Woodruff
+   *
+   */
+  public class RowFormatError extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Make an error with whatever message you want.
+     * 
+     * @param error
+     */
+    public RowFormatError(String error) {
+      super(error);
+    }
+  }
+
+  private Map<String, List<Number>> columnMap;
   private List<String> columnNames;
-  private Map<String, List<Double>> columnMap;
   private List<List<Number>> data; // List of rows
+
+  /**
+   * Add a column to the table.
+   * 
+   * @param columnName
+   */
+  public void addColumn(String columnName) {
+    columnNames.add(columnName);
+    for (List<Number> row : data) {
+      row.add(0);
+    }
+  }
+
+  /**
+   * Add a row to the data table. Throws error if length does not match column
+   * header length.
+   * 
+   * @param row
+   * @throws RowFormatError
+   */
+  public void addRow(List<Number> row) throws RowFormatError {
+    if (row.size() == columnNames.size()) {
+      data.add(row);
+    } else {
+      throw new RowFormatError("Size of row does match number of columns defined.");
+    }
+  }
+
+  /**
+   * @return columnMap
+   */
+  public Map<String, List<Number>> getColumnMap() {
+    return columnMap;
+  }
 
   /**
    * @return columnNames
@@ -22,39 +75,9 @@ public class DataTable {
   }
 
   /**
-   * @param columnNames
-   */
-  public void setColumnNames(List<String> columnNames) {
-    this.columnNames = columnNames;
-  }
-
-  /**
-   * @return columnMap
-   */
-  public Map<String, List<Double>> getColumnMap() {
-    return columnMap;
-  }
-
-  /**
-   * @return columns
+   * @return data
    */
   public List<List<Number>> getData() {
     return data;
-  }
-
-  public void addRow(List<Number> row) throws RowFormatError {
-    if (row.size() == columnNames.size()) {
-      data.add(row);
-    } else {
-      throw new RowFormatError("Size of row does match number of columns defined.");
-    }
-  }
-
-  public class RowFormatError extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public RowFormatError(String error) {
-      super(error);
-    }
   }
 }
