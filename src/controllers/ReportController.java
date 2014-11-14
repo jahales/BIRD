cd33dc02c297e06ccac312c1063ba04f3ee04c48
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-14 02:00:02 -0700

    Fized spacing and removed System.out lines to keep i/o clean.

--- src/controllers/ReportController.java
+++ src/controllers/ReportController.java
@@ -26,106 +26,95 @@ import javafx.scene.input.MouseEvent;
  *
  */
 public class ReportController {
+  DataTable table;
 
-    DataTable table;
+  @FXML
+  private LineChart<Number, Number> graph;
 
-    @FXML
-    private LineChart<Number, Number> graph;
+  @FXML
+  private NumberAxis yAxis;
 
-    @FXML
-    private NumberAxis yAxis;
+  @FXML
+  private NumberAxis xAxis;
 
-    @FXML
-    private NumberAxis xAxis;
+  @FXML
+  private ListView<String> xAxisChoices;
+  ObservableList<String> xList;
 
-    @FXML
-    private ListView<String> xAxisChoices;
-    ObservableList<String> xList;
+  @FXML
+  private TextField xMax;
 
-    @FXML
-    private TextField xMax;
+  @FXML
+  private TextField xMin;
 
-    @FXML
-    private TextField xMin;
+  @FXML
+  private ListView<String> yAxisChoices;
+  ObservableList<String> yList;
 
-    @FXML
-    private ListView<String> yAxisChoices;
-    ObservableList<String> yList;
-
-    /**
-     * initialize is called during FXMLoader call
-     */
-    public void initialize() {
-	try {
-	    table = CSVReader.loadCSV("A file");
-	} catch (IOException e) {
-	    System.out.println("File wasn't read right");
-	}
-
-	// X axis
-	// xAxisChoices.setItems((ObservableList<String>)
-	// table.getColumnNames());
-	xList = FXCollections.observableArrayList("1 fish", "2 fish", "Red fish", "Blue fish");
-	xAxisChoices.setItems(xList);
-	xAxisChoices.setOnMouseClicked(doubleClick);
-
-	// Y axis
-	// yAxisChoices.setItems((ObservableList<String>)
-	// table.getColumnNames());
-	yList = FXCollections.observableArrayList("I am Sam", "Sam I am");
-	yAxisChoices.setItems(yList);
-	yAxisChoices.setOnMouseClicked(doubleClick);
-
-	setEnterEvent(xMin, xList);
-	setEnterEvent(xMax, yList);
+  /**
+   * initialize is called during FXMLoader call
+   */
+  public void initialize() {
+    try {
+      table = CSVReader.loadCSV("A file");
+    } catch (IOException e) {
+      //System.out.println("File wasn't read right");
     }
 
-    /**
-     * Generic event setter for a given axis min/max and list
-     *
-     * @param boundedAxis
-     * @param axisListView
-     */
-    private void setEnterEvent(TextField boundedAxis,
-	    ObservableList<String> axisListView) {
-	boundedAxis.setOnKeyPressed(new EventHandler<KeyEvent>() {
-	    @Override
-	    public void handle(KeyEvent event) {
-		if (event.getCode() == KeyCode.ENTER) {
-		    // Update axis bounds
-		    double d = 0.0;
-		    try {
-			d = Double.parseDouble(boundedAxis.getText());
-			System.out.println(Double.toString(d) + " is a number");
-		    } catch (NumberFormatException e) {
-			System.out.println(Double.toString(d)
-				+ " is not a number");
-		    }
-		}
-	    }
-	});
+    // X axis
+    // xAxisChoices.setItems((ObservableList<String>)
+    // table.getColumnNames());
+    xList = FXCollections.observableArrayList("1 fish", "2 fish", "Red fish", "Blue fish");
+    xAxisChoices.setItems(xList);
+    xAxisChoices.setOnMouseClicked(doubleClick);
+
+    // Y axis
+    // yAxisChoices.setItems((ObservableList<String>)
+    // table.getColumnNames());
+    yList = FXCollections.observableArrayList("I am Sam", "Sam I am");
+    yAxisChoices.setItems(yList);
+    yAxisChoices.setOnMouseClicked(doubleClick);
+
+    setEnterEvent(xMin, xList);
+    setEnterEvent(xMax, yList);
+  }
+
+  /**
+   * Generic event setter for a given axis min/max and list
+   *
+   * @param boundedAxis
+   * @param axisListView
+   */
+  private void setEnterEvent(TextField boundedAxis, ObservableList<String> axisListView) {
+    boundedAxis.setOnKeyPressed(new EventHandler<KeyEvent>() {
+      @Override
+      public void handle(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+          // Update axis bounds
+        }
+      }
+    });
+  }
+
+  private EventHandler<MouseEvent> doubleClick = new EventHandler<MouseEvent>() {
+    @Override
+    public void handle(MouseEvent event) {
+      if (event.getClickCount() == 2) {
+        String value = xAxisChoices.selectionModelProperty().get().getSelectedItem();
+
+        XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+        series.setName(value);
+
+        for (double i = 0; i < 2 * Math.PI; i += 0.05) {
+          XYChart.Data<Number, Number> data;
+          data = new XYChart.Data<Number, Number>(i, Math.sin(i));
+          ErrorBar errorBar = new ErrorBar(3 * i);
+          data.setNode(errorBar);
+          series.getData().add(data);
+        }
+
+        graph.getData().add(series);
+      }
     }
-
-    private EventHandler<MouseEvent> doubleClick = new EventHandler<MouseEvent>() {
-	@Override
-	public void handle(MouseEvent event) {
-	    if (event.getClickCount() == 2) {
-		String value = xAxisChoices.selectionModelProperty().get()
-			.getSelectedItem();
-
-		XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
-		series.setName(value);
-
-		for (double i = 0; i < 2 * Math.PI; i += 0.05) {
-		    XYChart.Data<Number, Number> data;
-		    data = new XYChart.Data<Number, Number>(i, Math.sin(i));
-		    ErrorBar errorBar = new ErrorBar(3 * i);
-		    data.setNode(errorBar);
-		    series.getData().add(data);
-		}
-
-		graph.getData().add(series);
-	    }
-	}
-    };
+  };
 }
