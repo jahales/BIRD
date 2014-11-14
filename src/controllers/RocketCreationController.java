4efd1b8713b633a281b88fece27369030398024e
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-14 01:58:48 -0700

    Organized 2 of the main tabs contents and added some functions.

--- src/controllers/RocketCreationController.java
+++ src/controllers/RocketCreationController.java
@@ -1,21 +1,37 @@
 package controllers;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 
 /**
- *
- * @author Jacob
+ * Controller for the rocket creation view
+ * 
+ * @author Jacob, Brian
  */
 public class RocketCreationController {
-
     @FXML
     private TreeView<?> partList;
+    
+    @FXML
+    private AnchorPane partViewer;
 
     @FXML
     void addPart(ActionEvent event) {
 
     }
-
+    
+    public void initialize() {
+      try {
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("/views/parts/Fin.fxml"));
+        partViewer.getChildren().add(root);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 }
--- src/controllers/SimulationController.java
+++ src/controllers/SimulationController.java
@@ -2,43 +2,67 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-/**
- * Controller for Simulation view.
- * User inputs appropriate files for simulator.
- * Call run function to run simulation.
- * 
- * @author Brian Woodruff
- *
- */
 public class SimulationController {
+  @FXML
+  private Label engineFilePath;
 
-    @FXML
-    private TextField engineFile;
+  @FXML
+  private TextField length;
 
-    @FXML
-    private TextField length;
+  @FXML
+  private TextField asimuthAngle;
 
-    @FXML
-    private TextField asimuthAngle;
+  @FXML
+  private TextField polarAngle;
 
-    @FXML
-    private TextField polarAngle;
+  @FXML
+  private Label atmosphereFilePath;
 
-    @FXML
-    private TextField rocketFile;
+  @FXML
+  private Label launchRailFilePath;
 
-    @FXML
-    private TextField AtmosphereFile;
+  @FXML
+  private Label rocketFilePath;
 
-    /**
-     * Run simulation.
-     * 
-     * @param event
-     */
-    @FXML
-    void run(ActionEvent event) {
+  @FXML
+  private ChoiceBox<String> lengthUnits;
 
-    }
+  @FXML
+  private ChoiceBox<String> polarAngleUnits;
+
+  @FXML
+  private ChoiceBox<String> azimuthAngleUnits;
+
+  @FXML
+  void runSimulation(ActionEvent event) {
+
+  }
+
+  @FXML
+  void openRocketFile(ActionEvent event) {
+
+  }
+
+  @FXML
+  void openAtmosphereFile(ActionEvent event) {
+
+  }
+
+  @FXML
+  void openEngineFile(ActionEvent event) {
+
+  }
+
+  @FXML
+  void openLaunchRail(ActionEvent event) {
+
+  }
+
+  public void initialize() {
+
+  }
 }
