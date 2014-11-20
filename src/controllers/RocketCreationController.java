0ac773229149c2f0bc54623a2efdc7d4f4a35de6
Brian Woodruff <m.brian.woodruff@gmail.com>
2014-11-19 23:01:19 -0700

    Fixed some issues with rocket builder view.

--- src/controllers/RocketCreationController.java
+++ src/controllers/RocketCreationController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,11 +22,25 @@ import javafx.scene.layout.AnchorPane;
  * @author Jacob, Brian
  */
 public class RocketCreationController {
+  private final class RocketPartTreeItem extends TreeItem<RocketPart> {
+    private String name;
+    
+    public RocketPartTreeItem(String name) {
+      this.name = name;
+      System.out.println("Rocket tree part item Constructor");
+    }
+    
+    @Override
+    public String toString() {
+      System.out.println("Rocket tree part item toString()");
+      return name;
+    }
+  }
 
   /**
    *
    */
-  public enum RocketPart {
+   public enum RocketPart {
 
     /**
      *
@@ -57,9 +72,16 @@ public class RocketCreationController {
      */
     Parachute
   };
+
+  private TreeItem<RocketPart> treeViewRoot = new TreeItem<RocketPart>();
   
-  private TreeItem<RocketPart> treeViewRoot;
+  private RocketPartTreeItem internalTreePartsRoot = new RocketPartTreeItem("Internal");
+  private RocketPartTreeItem externalTreePartsRoot = new RocketPartTreeItem("External");
   
+//  private TreeItem<RocketPart> internalTreePartsRoot = new TreeItem<RocketPart>();
+//  private TreeItem<RocketPart> externalTreePartsRoot = new TreeItem<RocketPart>();
+  
+  private Map<RocketPart, URL> itemURL = new HashMap<RocketPart, URL>();
   private Map<RocketPart, Parent> itemParent = new HashMap<RocketPart, Parent>();
 
   @FXML
@@ -72,14 +94,15 @@ public class RocketCreationController {
    * Open PartChooser dialog window and add chosen part to tree view
    * 
    * @param event
+   * @throws IOException 
    */
   @FXML
-  void addPart(ActionEvent event) {
+  void addPart(ActionEvent event) throws IOException {
     PartChooser partChooser = new PartChooser();
     RocketPart part = partChooser.showPartDialog(partViewer.getScene().getWindow());
-    TreeItem<RocketPart> newPart = new TreeItem<RocketPart>(part);
-    if (newPart != null) {
-      treeViewRoot.getChildren().add(newPart);
+    if (part != null) {
+      treeViewRoot.getChildren().add(new TreeItem<RocketPart>(part));
+      itemParent.put(part, (Parent) FXMLLoader.load(itemURL.get(part)));
     }
   }
 
@@ -87,7 +110,7 @@ public class RocketCreationController {
     @Override
     public void changed(ObservableValue<? extends TreeItem<RocketPart>> arg0, TreeItem<RocketPart> arg1, TreeItem<RocketPart> arg2) {
       partViewer.getChildren().clear();
-      if (itemParent.get(arg2.getValue()) != null) {
+      if (itemURL.get(arg2.getValue()) != null) {
         partViewer.getChildren().add(itemParent.get(arg2.getValue()));
       }
     }
@@ -99,12 +122,12 @@ public class RocketCreationController {
    * @throws IOException
    */
   private void setMaps() throws IOException {
-    itemParent.put(RocketPart.CircularCylinder, (Parent) FXMLLoader.load(getClass().getResource("/views/parts/CircularCylinder.fxml")));
-    itemParent.put(RocketPart.ConicalFrustum,   (Parent) FXMLLoader.load(getClass().getResource("/views/parts/ConicalFrustum.fxml")));
-    itemParent.put(RocketPart.Motor,            (Parent) FXMLLoader.load(getClass().getResource("/views/parts/Motor.fxml")));
-    itemParent.put(RocketPart.NoseCone,         (Parent) FXMLLoader.load(getClass().getResource("/views/parts/NoseCone.fxml")));
-    itemParent.put(RocketPart.Parachute,        (Parent) FXMLLoader.load(getClass().getResource("/views/parts/Parachute.fxml")));
-    itemParent.put(RocketPart.TrapezoidFinSet,  (Parent) FXMLLoader.load(getClass().getResource("/views/parts/TrapezoidFinSet.fxml")));
+    itemURL.put(RocketPart.CircularCylinder, getClass().getResource("/views/parts/CircularCylinder.fxml"));
+    itemURL.put(RocketPart.ConicalFrustum,   getClass().getResource("/views/parts/ConicalFrustum.fxml"));
+    itemURL.put(RocketPart.Motor,            getClass().getResource("/views/parts/Motor.fxml"));
+    itemURL.put(RocketPart.NoseCone,         getClass().getResource("/views/parts/NoseCone.fxml"));
+    itemURL.put(RocketPart.Parachute,        getClass().getResource("/views/parts/Parachute.fxml"));
+    itemURL.put(RocketPart.TrapezoidFinSet,  getClass().getResource("/views/parts/TrapezoidFinSet.fxml"));
   }
 
   /**
@@ -115,8 +138,10 @@ public class RocketCreationController {
   public void initialize() throws IOException {
     setMaps();
     
-    treeViewRoot = new TreeItem<RocketPart>();
     treeViewRoot.setExpanded(true);
+    treeViewRoot.getChildren().add(internalTreePartsRoot);
+    treeViewRoot.getChildren().add(externalTreePartsRoot);
+    
     partList.getSelectionModel().selectedItemProperty().addListener(selectionEvent);
     partList.setRoot(treeViewRoot);
     partList.setShowRoot(false);
