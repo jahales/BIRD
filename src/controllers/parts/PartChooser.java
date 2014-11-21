720f1ae5e628a20a50f938dbf701bb6a03ed8da2
Joseph Hales <joseph.r.hales@gmail.com>
2014-11-21 13:34:44 -0700

    Added enter key and double click events to add part menu.

--- src/controllers/parts/PartChooser.java
+++ src/controllers/parts/PartChooser.java
@@ -4,9 +4,13 @@ import controllers.RocketCreationController.RocketPart;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
@@ -18,6 +22,7 @@ import javafx.stage.Window;
  * @author Jacob
  */
 public class PartChooser {
+
   private RocketPart selectedRocketPart;
   private Stage stage;
   private ListView<RocketPart> partsList;
@@ -31,27 +36,44 @@ public class PartChooser {
     partsList = new ListView<RocketPart>();
     partsList.setItems(FXCollections.observableArrayList(RocketPart.values()));
     partsList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<RocketPart>() {
-          @Override
-          public void changed(ObservableValue<? extends RocketPart> arg0, RocketPart arg1, RocketPart arg2) {
-            selectedRocketPart = arg2;
-          }
-        });
-    
+      @Override
+      public void changed(ObservableValue<? extends RocketPart> arg0, RocketPart arg1, RocketPart arg2) {
+        selectedRocketPart = arg2;
+      }
+    });
+
     VBox vbox = new VBox();
     HBox hbox = new HBox(10);
-    
+
     Button addPart = new Button("Add Part");
     Button cancel = new Button("Cancel");
-    
+
     hbox.getChildren().addAll(addPart, cancel);
     vbox.getChildren().addAll(partsList, hbox);
-    
+
     stage = new Stage();
     stage.setTitle("Add part");
     stage.initOwner(window);
     stage.initModality(Modality.WINDOW_MODAL);
     stage.setScene(new Scene(vbox));
-    
+
+    final EventHandler<KeyEvent> keyEventHandler
+      = new EventHandler<KeyEvent>() {
+        public void handle(final KeyEvent keyEvent) {
+          if (keyEvent.getCode() == KeyCode.ENTER) {
+            stage.close();
+          }
+        }
+      };
+    partsList.setOnKeyPressed(keyEventHandler);
+
+    partsList.setOnMouseClicked((e) -> {
+      if (e.getButton().equals(MouseButton.PRIMARY)) {
+        if (e.getClickCount() == 2) {
+          stage.close();
+        }
+      }
+    });
     addPart.setOnAction((e) -> {
       stage.close();
     });
@@ -63,9 +85,9 @@ public class PartChooser {
       selectedRocketPart = null;
       stage.close();
     });
-    
+
     stage.showAndWait();
-    
+
     return selectedRocketPart;
   }
 }
