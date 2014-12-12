feae5ca508d8ea8b3c9a4514f2512350b9cc35ae
Joseph Hales <joseph.r.hales@gmail.com>
2014-12-12 13:23:25 -0700

    Finalized motor fileChooser.

--- src/models/FileHelper.java
+++ src/models/FileHelper.java
@@ -133,8 +133,7 @@ public class FileHelper {
     FileChooser fileChooser = new FileChooser();
     fileChooser.setTitle("Open Resource File");
     // Set initial file path
-    // TODO: Move file directories to appsettings, and away from mainViewModel.
-    // configInitialDirectory(fileChooser, mainViewModel);
+    configInitialDirectory(fileChooser);
     fileChooser.getExtensionFilters().add(new ExtensionFilter("XML file", "*.xml"));
     openFile = fileChooser.showOpenDialog(root.getScene().getWindow());
     if (openFile != null) {
